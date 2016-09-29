71d261773e5853247e4a2d82c1d296f7067b1bbb
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-09-29 20:25:09 +0100

    fix tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var detect = require('feature-detect-es6')
 var TestRunner = require('test-runner')
 
-if (detect.all('class', 'arrowFunction', 'let', 'const')) {
+if (detect.all('class', 'arrowFunction', 'let', 'const', 'spread')) {
   TestRunner.run('src/test/core-*.js')
 } else {
   require('core-js/es6/object')
